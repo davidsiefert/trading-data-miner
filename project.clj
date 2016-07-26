a281f1e5ca45208a380eb0f09d2fc5ece11a62c3
David Siefert <siefert.david@gmail.com>
2016-07-26 06:36:14 -0500

    Fixing repl load issue.
    Cannot use a string for the :main name.  Must be a symbol.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject trading-data-miner "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]]
-  :main "trading-data-miner.core")
+  :main trading-data-miner.core)
